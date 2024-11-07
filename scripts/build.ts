6bb846ceb70048b7bebb212fc4b44f6d4e462041
Sukairo-02 <sreka9056@gmail.com>
2024-11-07 18:36:06 +0200

    Reconfigured build for `tsup`

---
+++ scripts/build.ts
@@ -0,0 +1,50 @@
+import 'zx/globals';
+
+import { build } from 'tsup';
+
+fs.removeSync('dist');
+
+await build({
+	entry: ['src/index.ts'],
+	splitting: false,
+	sourcemap: true,
+	dts: true,
+	format: ['cjs', 'esm'],
+	bundle: false,
+	outExtension(ctx) {
+		if (ctx.format === 'cjs') {
+			return {
+				dts: '.d.cts',
+				js: '.cjs',
+			};
+		}
+		return {
+			dts: '.d.ts',
+			js: '.js',
+		};
+	},
+});
+
+await build({
+	entry: ['src/neo.ts'],
+	splitting: false,
+	sourcemap: true,
+	dts: true,
+	format: ['cjs', 'esm'],
+	bundle: false,
+	outExtension(ctx) {
+		if (ctx.format === 'cjs') {
+			return {
+				dts: '.d.cts',
+				js: '.cjs',
+			};
+		}
+		return {
+			dts: '.d.ts',
+			js: '.js',
+		};
+	},
+});
+
+fs.copyFileSync('package.json', 'dist/package.json');
+fs.copyFileSync('README.md', 'dist/README.md');
