561cbd1aaf8e96bf65171f43bdadcdabfd0b0f71
AndriiSherman <andreysherman11@gmail.com>
2025-08-04 16:34:58 +0300

    dprint

--- integration-tests/tests/sqlite/expo-sqlite/App.tsx
+++ integration-tests/tests/sqlite/expo-sqlite/App.tsx
@@ -4,51 +4,50 @@ import { StatusBar } from 'expo-status-bar';
 // @ts-expect-error
 import { useEffect, useState } from 'react';
 // @ts-expect-error
-import { StyleSheet, Text, View } from 'react-native';
-import { allDataTypesSqlStreamTest, allDataTypesSqlValuesTest, allDataTypesUnsafeTest } from './tests.ts';
 import * as SQLite from 'expo-sqlite';
+import { StyleSheet, Text, View } from 'react-native';
 import { waddler } from 'waddler/expo-sqlite';
+import { allDataTypesSqlStreamTest, allDataTypesSqlValuesTest, allDataTypesUnsafeTest } from './tests.ts';
 
 const client = SQLite.openDatabaseSync('inMemoryDb', {}, ':memory:');
-const sql = waddler({client});
+const sql = waddler({ client });
 
 export default function App() {
-  const [items, setItems] = useState<number>(0);
-  const tests = [allDataTypesUnsafeTest,allDataTypesSqlValuesTest,allDataTypesSqlStreamTest];
+	const [items, setItems] = useState<number>(0);
+	const tests = [allDataTypesUnsafeTest, allDataTypesSqlValuesTest, allDataTypesSqlStreamTest];
 
-  useEffect(() => {
-    (async () => {
-      let passed = 0;
-      try{
-        for (const test of tests) {
-          await test(sql);
-          passed += 1;
-        }
-      
-      }catch (error) {
-        console.error((error as Error).name);
-        console.error((error as Error).message);
-        console.error((error as Error).cause);
-        console.error((error as Error).stack);
-      }
+	useEffect(() => {
+		(async () => {
+			let passed = 0;
+			try {
+				for (const test of tests) {
+					await test(sql);
+					passed += 1;
+				}
+			} catch (error) {
+				console.error((error as Error).name);
+				console.error((error as Error).message);
+				console.error((error as Error).cause);
+				console.error((error as Error).stack);
+			}
 
-      setItems(passed)
-    })()
-  })
+			setItems(passed);
+		})();
+	});
 
-  return (
-    <View style={styles.container}>
-      <Text>Tests	{tests.length - items} failed | {items} passed ({tests.length}){}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Tests{tests.length - items} failed | {items} passed ({tests.length}){}</Text>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
--- integration-tests/tests/sqlite/op-sqlite/App.tsx
+++ integration-tests/tests/sqlite/op-sqlite/App.tsx
@@ -1,58 +1,58 @@
 // This file serves only as a reference for what and how I'm testing in a detached project.
-import './global.js'
+import './global.js';
 // @ts-expect-error
 import { StatusBar } from 'expo-status-bar';
 // @ts-expect-error
 import { useEffect, useState } from 'react';
 // @ts-expect-error
-import { StyleSheet, Text, View } from 'react-native';
-import { allDataTypesSqlValuesTest, allDataTypesUnsafeTest } from './tests.ts';
 import { open } from '@op-engineering/op-sqlite';
+import { StyleSheet, Text, View } from 'react-native';
 import { waddler } from 'waddler/op-sqlite';
+import { allDataTypesSqlValuesTest, allDataTypesUnsafeTest } from './tests.ts';
 
 const client = open({
-  name: 'inMemoryDb',
-  location: ':memory:',
+	name: 'inMemoryDb',
+	location: ':memory:',
 });
-const sql = waddler({client});
+const sql = waddler({ client });
 
 export default function App() {
-  const [items, setItems] = useState<number>(0);
-  const tests = [allDataTypesUnsafeTest, allDataTypesSqlValuesTest];
+	const [items, setItems] = useState<number>(0);
+	const tests = [allDataTypesUnsafeTest, allDataTypesSqlValuesTest];
 
-  useEffect(() => {
-    (async () => {
-      let passed = 0;
-      try{
-        for (const test of tests) {
-          await test(sql);
-          passed += 1;
-        }
-      }catch (error) {
-        console.error((error as Error).name);
-        console.error((error as Error).message);
-        console.error((error as Error).cause);
-        console.error((error as Error).stack);
-      }
+	useEffect(() => {
+		(async () => {
+			let passed = 0;
+			try {
+				for (const test of tests) {
+					await test(sql);
+					passed += 1;
+				}
+			} catch (error) {
+				console.error((error as Error).name);
+				console.error((error as Error).message);
+				console.error((error as Error).cause);
+				console.error((error as Error).stack);
+			}
 
-      setItems(passed)
-    })()
-  })
+			setItems(passed);
+		})();
+	});
 
-  return (
-    // @ts-expect-error
-    <View style={styles.container}>
-      <Text>Tests	{tests.length - items} failed | {items} passed ({tests.length}){}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		// @ts-expect-error
+		<View style={styles.container}>
+			<Text>Tests{tests.length - items} failed | {items} passed ({tests.length}){}</Text>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
